16bed0e0ddf08e3fb01ae1be199f413573906494
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2024-07-04 22:38:53 +0900

    V0.2.0 (#1)
    * v0.2.0
    
    * v0.2.0
    
    ---------
    
    Co-authored-by: yudai-tiny-developer <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -1,58 +1,70 @@
-const _pretend_not_to_watch_app = document.querySelector('ytd-app') ?? document.body;
-
-function click_remove_history_button(iframe, video_id) {
-    for (const button_renderer of iframe.contentDocument.querySelectorAll('ytd-video-renderer ytd-button-renderer')) {
-        if (button_renderer.data.serviceEndpoint.feedbackEndpoint.contentId === video_id) {
-            const button = button_renderer.querySelector('button');
-            if (button) {
-                button.click();
-                return true;
+const _pretend_not_to_watch_app = document.querySelector('ytd-app');
+if (_pretend_not_to_watch_app) {
+    function click_remove_history_button(iframe, video_id) {
+        for (const button_renderer of iframe.contentDocument.querySelectorAll('ytd-video-renderer ytd-button-renderer')) {
+            if (button_renderer.data.serviceEndpoint.feedbackEndpoint.contentId === video_id) {
+                const button = button_renderer.querySelector('button');
+                if (button) {
+                    button.click();
+                    return true;
+                }
             }
         }
-    }
-    return false;
-}
-
-document.addEventListener('_pretend_not_to_watch_request', e => {
-    const iframe = document.body.querySelector('iframe#_pretend_not_to_watch');
-    if (!iframe) {
-        return;
+        return false;
     }
 
-    const player = _pretend_not_to_watch_app.querySelector('div#movie_player');
-    if (!player) {
-        return;
+    function open_toast(simpleText) {
+        _pretend_not_to_watch_app.resolveCommand({
+            openPopupAction: {
+                popup: {
+                    notificationActionRenderer: {
+                        responseText: {
+                            simpleText
+                        }
+                    }
+                },
+                popupType: 'TOAST'
+            }
+        });
     }
 
-    const video_data = player.getVideoData();
-    if (!video_data) {
-        return;
-    }
+    document.addEventListener('_pretend_not_to_watch_request', e => {
+        const iframe = document.body.querySelector('iframe#_pretend_not_to_watch');
+        if (!iframe) {
+            return;
+        }
 
-    iframe.onload = () => {
-        console.log(`onload: ${iframe.contentDocument.querySelector('ytd-video-renderer ytd-button-renderer')}`);
-        if (click_remove_history_button(iframe, video_data.video_id)) {
-            console.log('Succeeded');
-        } else {
+        const player = _pretend_not_to_watch_app.querySelector('div#movie_player');
+        if (!player) {
+            return;
+        }
+
+        const video_data = player.getVideoData();
+        if (!video_data) {
+            return;
+        }
+
+        const interval = setInterval(() => {
+            iframe.contentWindow.location.reload();
+        }, 2000);
+
+        iframe.onload = () => {
             const observer = new MutationObserver((mutations, observer) => {
-                console.log('Mutation...');
                 if (click_remove_history_button(iframe, video_data.video_id)) {
+                    clearInterval(interval);
                     observer.disconnect();
-                    console.log('Succeeded');
+                    document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_succeeded'));
+                    open_toast(e.detail.toast);
                 }
             });
-
             observer.observe(iframe.contentDocument, { childList: true, subtree: true });
-
             setTimeout(() => {
-                console.log('Timeout');
+                clearInterval(interval);
                 observer.disconnect();
-            }, 4000);
-        }
-    };
+                document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_timeout'));
+            }, 5000);
+        };
+    });
 
-    console.log('Reload History');
-    iframe.contentWindow.location.reload();
-});
-
-document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_init'));
+    document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_init'));
+}
