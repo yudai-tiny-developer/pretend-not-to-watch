641cd7c5d7d89184e37d2eba10c4141c11c27062
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-12-07 07:28:05 +0900

    V0.5.1 (#4)
    * ui
    
    * commands
    
    * feedbackTokens
    
    ---------
    
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

--- inject.js
+++ inject.js
@@ -11,7 +11,7 @@
         return `${TIMESTAMP_MS}_${digest}`;
     };
 
-    async function getHistoryTokens() {
+    async function getHistoryTokens(targetVideoId) {
         const res = await fetch("https://www.youtube.com/youtubei/v1/browse?key=" + ytcfg.data_.INNERTUBE_API_KEY + "&prettyPrint=false", {
             "headers": {
                 "accept": "*/*",
@@ -36,15 +36,21 @@
         for (const section of sections) {
             const items = section?.itemSectionRenderer?.contents || [];
             for (const item of items) {
-                const endpoint = item?.lockupViewModel?.metadata?.lockupMetadataViewModel?.menuButton?.buttonViewModel?.onTap?.innertubeCommand?.showSheetCommand?.panelLoadingStrategy?.inlineContent?.sheetViewModel?.content?.listViewModel?.listItems[5]?.listItemViewModel?.rendererContext?.commandContext?.onTap?.innertubeCommand?.feedbackEndpoint;
-                if (endpoint) tokens.push({ videoId: endpoint.contentId, token: endpoint.feedbackToken });
+                const commands = item?.lockupViewModel?.metadata?.lockupMetadataViewModel?.menuButton?.buttonViewModel?.onTap?.innertubeCommand?.showSheetCommand?.panelLoadingStrategy?.inlineContent?.sheetViewModel?.content?.listViewModel.listItems || [];
+                for (const command of commands) {
+                    if (command?.listItemViewModel?.title?.content === 'Remove from watch history') {
+                        const endpoint = command?.listItemViewModel?.rendererContext?.commandContext?.onTap?.innertubeCommand?.feedbackEndpoint;
+                        if (endpoint?.contentId === targetVideoId) tokens.push(endpoint.feedbackToken);
+                        break;
+                    }
+                }
             }
         }
 
         return tokens;
     }
 
-    async function deleteHistory(feedbackToken) {
+    async function deleteHistory(feedbackTokens) {
         const res = await fetch("https://www.youtube.com/youtubei/v1/feedback?key=" + ytcfg.data_.INNERTUBE_API_KEY + "&prettyPrint=false", {
             "headers": {
                 "accept": "*/*",
@@ -58,7 +64,7 @@
                         "clientVersion": ytcfg.data_.INNERTUBE_CLIENT_VERSION,
                     },
                 },
-                "feedbackTokens": [feedbackToken],
+                "feedbackTokens": feedbackTokens,
             }),
             "method": "POST",
         });
@@ -68,21 +74,19 @@
     document.addEventListener('_pretend_not_to_watch_request', async e => {
         const targetVideoId = e.detail;
 
-        const tokens = await getHistoryTokens();
-
-        const entry = tokens.find((t) => t.videoId === targetVideoId);
-        if (!entry) {
-            document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_timeout'));
+        const tokens = await getHistoryTokens(targetVideoId);
+        if (tokens?.length === 0) {
+            document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_succeeded')); // already removed
             return;
         }
 
-        const result = await deleteHistory(entry.token);
-        if (!result) {
-            document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_timeout'));
+        const result = await deleteHistory(tokens);
+        if (result?.feedbackResponses[0]?.isProcessed) {
+            document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_succeeded'));
             return;
         }
 
-        document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_succeeded'));
+        document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_timeout'));
     });
 
     document.dispatchEvent(new CustomEvent('_pretend_not_to_watch_init'));
